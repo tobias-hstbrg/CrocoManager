98d1bcf7d51532f03a98f785f71530e23d6084aa
Tobias Hesterberg <tobiashesterberg3103@gmail.com>
2026-01-21 12:35:10 +0100

    created base service with a GetAll Method

---
+++ Services/BaseService.cs
@@ -0,0 +1,25 @@
+﻿using Supabase.Postgrest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrocoManager.Services
+{
+    public abstract class BaseService<T> where T : BaseModel, new()
+    {
+        protected readonly SupabaseClientService _supabaseClient;
+
+        public BaseService(SupabaseClientService supabaseClient )
+        {
+            _supabaseClient = supabaseClient;
+        }
+
+        public virtual async Task<List<T>> GetAllAsync()
+        {
+            var response = await _supabaseClient.Client.From<T>().Get();
+            return response.Models;
+        }
+    }
+}
