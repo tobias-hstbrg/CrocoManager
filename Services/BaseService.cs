9214ce944775616cf9f3a58558f31552888cb9cf
Tobias Hesterberg <tobiashesterberg3103@gmail.com>
2026-01-21 13:53:55 +0100

    remaining CRUD generic methods from the class diagram

--- Services/BaseService.cs
+++ Services/BaseService.cs
@@ -21,5 +21,37 @@ namespace CrocoManager.Services
             var response = await _supabaseClient.Client.From<T>().Get();
             return response.Models;
         }
+
+        public virtual async Task<T?> GetByIdAsync(Guid id)
+        {
+            var response = await _supabaseClient.Client.From<T>().Filter("id", Supabase.Postgrest.Constants.Operator.Equals, id).Single();
+            return response;
+
+        }
+
+        public virtual async Task<T?> AddAsync(T obj)
+        {
+            var response = await _supabaseClient.Client.From<T>().Insert(obj);
+            return response.Models.FirstOrDefault();
+        }
+
+        public virtual async Task<T?> UpdateAsync(T obj)
+        {
+            var response = await _supabaseClient.Client.From<T>().Update(obj);
+            return response.Models.FirstOrDefault();
+        }
+
+        public virtual async Task<bool> DeleteAsync(Guid id)
+        {
+            try
+            {
+                await _supabaseClient.Client.From<T>().Filter("id", Supabase.Postgrest.Constants.Operator.Equals, id).Delete();
+                return true;
+            }
+            catch (Exception ex) {
+                System.Diagnostics.Debug.WriteLine($"Delete failed: {ex.Message}");
+                return false;
+            }
+        }
     }
 }
