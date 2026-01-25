39f92768039463669dc72bbd7e5c64ca4c27c7f7
Tobias Hesterberg <tobiashesterberg3103@gmail.com>
2026-01-25 20:26:12 +0100

    grid prototype

--- Views/AnimalPage.xaml.cs
+++ Views/AnimalPage.xaml.cs
@@ -1,9 +1,48 @@
+using CrocoManager.Models;
+
 namespace CrocoManager.Views;
 
 public partial class AnimalPage : ContentPage
 {
-	public AnimalPage()
+    private bool isScrolling = false;
+    public List<Animal> Animals { get; set; }
+
+    public AnimalPage()
 	{
 		InitializeComponent();
-	}
+
+        Animals = new List<Animal>
+        {
+            new Animal { Name = "Charlie", Gender = "Männlich", Age = 8, Species = "Amerikanischer Alligator" },
+            new Animal { Name = "Rex", Gender = "Männlich", Age = 12, Species = "Spitzkrokodil" },
+            new Animal { Name = "Bella", Gender = "Weiblich", Age = 5, Species = "Nilkrokodil" },
+            new Animal { Name = "Max", Gender = "Männlich", Age = 15, Species = "Leistenkrokodil" },
+            new Animal { Name = "Luna", Gender = "Weiblich", Age = 3, Species = "Sumpfkrokodil" }
+        };
+
+        BindingContext = this;
+    }
+
+    private async void OnContentScrolled(object sender, ScrolledEventArgs e)
+    {
+        if (isScrolling) return;
+
+        isScrolling = true;
+        await headerScroll.ScrollToAsync(e.ScrollX, 0, false);
+        isScrolling = false;
+    }
+
+    private void OnEditClicked(object sender, EventArgs e)
+    {
+        var button = sender as Button;
+        var animal = button?.BindingContext as Animal;
+        // Handle edit
+    }
+
+    private void OnDeleteClicked(object sender, EventArgs e)
+    {
+        var button = sender as Button;
+        var animal = button?.BindingContext as Animal;
+        // Handle delete
+    }
 }
