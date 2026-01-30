dac5ac83915843bc6cd876e7a5e7a47933308f3d
Tobias Hesterberg <127434393+tobias-hstbrg@users.noreply.github.com>
2026-01-30 12:20:42 +0100

    Feat/feeding plan (#35)
    Implemented pages and logic for anmial feeding functionallity and fixed release build crashes.

---
+++ Services/FeedingPlanService.cs
@@ -0,0 +1,36 @@
+﻿using CrocoManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrocoManager.Services
+{
+    public class FeedingPlanService : BaseService<FeedingPlan>
+    {
+        public FeedingPlanService(SupabaseClientService supabaseClient)
+           : base(supabaseClient)
+        {
+        }
+
+        public async Task<bool> ToggleActiveAsync(Guid id)
+        {
+            FeedingPlan? plan = await GetByIdAsync(id);
+            if (plan == null) throw new NullReferenceException();
+            plan.IsActive = !plan.IsActive;
+            await UpdateAsync(plan);
+            return plan.IsActive;
+        }
+
+        public async Task<FeedingPlan> GetActivePlanAsync()
+        {
+            List<FeedingPlan> plans = await FilterByAsync("is_active", true);
+            if (plans.Count == 0)
+                throw new InvalidOperationException("No active feeding plan found.");
+            if(plans.Count > 1)
+                throw new InvalidOperationException("Multiple active feeding plans found.");
+            return plans[0];
+        }
+    }
+}
