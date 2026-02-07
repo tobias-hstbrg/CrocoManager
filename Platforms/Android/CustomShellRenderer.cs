2f42e912ca0f209e79ea1427cea7e1cad3c5292c
Tobias Hesterberg <127434393+tobias-hstbrg@users.noreply.github.com>
2026-02-07 19:01:54 +0100

    Remove TabBar Labels #55
    This removes TabBar Labels in the Maui TabBar with a custom shell renderer in the android platform folder

---
+++ Platforms/Android/CustomShellRenderer.cs
@@ -0,0 +1,36 @@
+﻿using Android.Content;
+using Google.Android.Material.BottomNavigation;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Handlers.Compatibility;
+using Microsoft.Maui.Controls.Platform;
+using Microsoft.Maui.Controls.Platform.Compatibility;
+
+namespace CrocoManager.Platforms.Android
+{
+    public class CustomShellRenderer : ShellRenderer
+    {
+        public CustomShellRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override IShellBottomNavViewAppearanceTracker CreateBottomNavViewAppearanceTracker(ShellItem shellItem)
+        {
+            return new CustomBottomNavViewAppearanceTracker(this, shellItem);
+        }
+    }
+
+    public class CustomBottomNavViewAppearanceTracker : ShellBottomNavViewAppearanceTracker
+    {
+        public CustomBottomNavViewAppearanceTracker(IShellContext shellContext, ShellItem shellItem)
+            : base(shellContext, shellItem)
+        {
+        }
+
+        public override void SetAppearance(BottomNavigationView bottomView, IShellAppearanceElement appearance)
+        {
+            base.SetAppearance(bottomView, appearance);
+
+            bottomView.LabelVisibilityMode = LabelVisibilityMode.LabelVisibilityUnlabeled;
+        }
+    }
+}
