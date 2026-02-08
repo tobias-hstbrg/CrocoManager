34cc17aef9f84bf7c8f91593f63a3436b09173ab
Tobias Hesterberg <127434393+tobias-hstbrg@users.noreply.github.com>
2026-02-08 21:48:47 +0100

    Feat/dashboard (#57)
    Viewmodel implementation to serve data to the homepage

--- Views/HomePage.xaml.cs
+++ Views/HomePage.xaml.cs
@@ -1,10 +1,11 @@
-using CrocoManager.ViewModel;
+﻿using CrocoManager.ViewModel;
 
 namespace CrocoManager.Views;
 
 public partial class HomePage : ContentPage
 {
-	public HomePage(HomeViewModel viewmodel)
+    private readonly HomeViewModel _viewModel;
+    public HomePage(HomeViewModel viewmodel)
 	{
 		InitializeComponent();
 		BindingContext = viewmodel;
